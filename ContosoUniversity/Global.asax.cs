888c3c3240639ab6fdd9aefbf0af804b7f6fdbb7
Mst Bithy <mstbithy.cse@gmail.com>
2021-07-03 02:09:33 +0600

    Updated

--- ContosoUniversity/Global.asax.cs
+++ ContosoUniversity/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ContosoUniversity.DAL;
+using System.Data.Entity.Infrastructure.Interception;
 
 namespace ContosoUniversity
 {
@@ -18,6 +20,8 @@ namespace ContosoUniversity
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             //Database.SetInitializer
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
         }
     }
 }
